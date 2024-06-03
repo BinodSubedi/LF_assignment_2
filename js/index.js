49d6a01c8b44b0ce23de4be68863cc6eac571cb5
binodsubedi125 <binodsubedi125@gmail.com>
2024-06-04 00:14:56 +0545

    changed the naming according to bem principles

--- js/index.js
+++ js/index.js
@@ -1,11 +1,39 @@
-const modeChanger = document.querySelector(".navigation_bar__face_icon__img");
-const menu_toggler = document.querySelector(".navigation_bar__menu_button");
-const menu = document.querySelector(".navigation_bar__elements");
+const modeChanger = document.querySelector(".navigation-bar__face-icon__img");
+const menu_toggler = document.querySelector(".navigation-bar__menu-button");
+const menu = document.querySelector(".navigation-bar__elements");
+const fotter_logo_div = document.querySelector('.footer__content-and-logo__logos');
+
+const upper_logos = document.querySelector('.social-media-logos');
 
 modeChanger.addEventListener('click',()=>{
+
+    const darkChoice_or_white = Array.from(document.body.classList).length;
+
     document.body.classList.toggle('dark_theme');
 
-    console.log(modeChanger.children[0])
+    const ligh_mode = ['linkedin_white.png', 'instagram_white.png', 'dribble_white.png', 'github_white.png'];
+
+    const dark_mode = ['linkedin_logo_black.png','instagram_logo_black.png','dribbble_logo_black.png','github_logo_black.png'];
+
+    let index = 0;
+
+    const fotter_logos =  Array.from(fotter_logo_div.childNodes);
+
+    Array.from(upper_logos.childNodes).map((val,i)=>{
+
+            switch(i){
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                    val.src = `./assets/icons/${darkChoice_or_white == 0 ? ligh_mode[index] : dark_mode[index]}`;
+                    fotter_logos[i].src = val.src;
+                    index++;
+                    break;
+           }
+
+
+    });
 })
 
 menu_toggler.addEventListener('click', ()=>{
@@ -13,6 +41,7 @@ menu_toggler.addEventListener('click', ()=>{
     menu.classList.toggle('hidden');
 
 })
+
 window.addEventListener('resize', (e) => {
 
     if(window.innerWidth > 750){
