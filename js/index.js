3ae01acdad731611428eaae0776d2d7c5e39669b
binodsubedi125 <binodsubedi125@gmail.com>
2024-06-03 08:36:07 +0545

    few adjustments for responsive design

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
-const modeChanger = document.querySelector(".navigation_bar__face_icon");
-
+const modeChanger = document.querySelector(".navigation_bar__face_icon__img");
+const menu_toggler = document.querySelector(".navigation_bar__menu_button");
+const menu = document.querySelector(".navigation_bar__elements");
 
 modeChanger.addEventListener('click',()=>{
     document.body.classList.toggle('dark_theme');
 
     console.log(modeChanger.children[0])
+})
+
+menu_toggler.addEventListener('click', ()=>{
+    menu.classList.toggle('hidden');
 })
